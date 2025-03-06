41970e32f6c2ae42c7111f92bca36bfcf3fc1304
nikolay <n.smolov@yandex.ru>
2025-03-06 14:14:50 +0300

    adds fiber server

--- pkg/sqldb/sqldb.go
+++ pkg/sqldb/sqldb.go
@@ -6,7 +6,12 @@ import (
 	"github.com/rs/zerolog"
 )
 
-func New(driver, dsn string, logger zerolog.Logger) *sql.DB {
+type SQLDB struct {
+	*sql.DB
+	logger zerolog.Logger
+}
+
+func New(driver, dsn string, logger zerolog.Logger) SQLDB {
 	logFatal := func(err error) {
 		logger.Fatal().Err(err).Caller().Send()
 	}
@@ -20,5 +25,13 @@ func New(driver, dsn string, logger zerolog.Logger) *sql.DB {
 		logFatal(err)
 	}
 	logger.Info().Str("driver", driver).Msg("databse is connected")
-	return db
+	return SQLDB{DB: db, logger: logger}
+}
+
+func (sqldb SQLDB) Close() {
+	if err := sqldb.DB.Close(); err != nil {
+		sqldb.logger.Warn().Err(err).Msg("closing database connections")
+	} else {
+		sqldb.logger.Info().Msg("database connections safely closed")
+	}
 }
